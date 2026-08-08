f4a59009eb67df3b945610418bf9c7466a192b72
David Mosquera Domínguez <dmosquera@MacBook-Air-de-David.local>
2026-08-08 17:41:18 +0200

    feat: inbox executor

---
+++ code/infrastructure/inbound/execution/inbox/src/main/java/com/architecture/hexagonal/infrastructure/inbound/execution/inbox/config/SchedulerConfig.java
@@ -0,0 +1,8 @@
+package com.architecture.hexagonal.infrastructure.inbound.execution.inbox.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig {}
---
+++ code/infrastructure/inbound/execution/inbox/src/test-integration/java/com/architecture/hexagonal/infrastructure/inbound/execution/inbox/executor/InboxSchedulerIntegrationTest.java
@@ -0,0 +1,53 @@
+package com.architecture.hexagonal.infrastructure.inbound.execution.inbox.executor;
+
+import com.architecture.hexagonal.application.port.configuration.SchedulerConfigurationPort;
+import com.architecture.hexagonal.domain.model.vo.SchedulerOutboxConfigurationVo;
+import com.architecture.hexagonal.infrastructure.inbound.execution.inbox.config.TestApplication;
+import com.architecture.hexagonal.infrastructure.inbound.execution.inbox.data.vo.SchedulerInboxConfigurationVoTestDataBuilder;
+import com.architecture.hexagonal.infrastructure.inbound.execution.inbox.executor.impl.InboxSchedulerImpl;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(classes = {TestApplication.class, InboxSchedulerImpl.class})
+class InboxSchedulerIntegrationTest {
+
+    @Autowired
+    private InboxSchedulerImpl inboxSchedulerImpl;
+
+    @MockitoBean
+    private SchedulerConfigurationPort schedulerConfigurationPort;
+
+    @MockitoBean
+    private ScheduledTaskRegistrar scheduledTaskRegistrar;
+
+    @Test
+    void shouldConfigureTasksWithCorrectInterval() {
+        SchedulerOutboxConfigurationVo schedulerConfig = SchedulerInboxConfigurationVoTestDataBuilder.builder().build().schedulerInboxConfigurationVo();
+
+        Mockito.when(schedulerConfigurationPort.getSchedulerOutbox()).thenReturn(schedulerConfig);
+
+        inboxSchedulerImpl.configureTasks(scheduledTaskRegistrar);
+
+        ArgumentCaptor<Runnable> scheduleProcessCaptor = ArgumentCaptor.forClass(Runnable.class);
+        ArgumentCaptor<Duration> durationCaptor = ArgumentCaptor.forClass(Duration.class);
+
+        Mockito.verify(scheduledTaskRegistrar).addFixedDelayTask(scheduleProcessCaptor.capture(), durationCaptor.capture());
+        Duration expectedDuration = schedulerConfig.getPollingInterval();
+        Duration capturedDuration = durationCaptor.getValue();
+        
+        assertEquals(expectedDuration, capturedDuration);
+
+        Mockito.verify(schedulerConfigurationPort).getSchedulerOutbox();
+
+        scheduleProcessCaptor.getValue().run();
+    }
+}
